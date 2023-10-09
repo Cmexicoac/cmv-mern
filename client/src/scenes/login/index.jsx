910b35a9b8ce5921975f2c6c90384ecf2d554852
RAAG0206 <raag0206@hotmail.com>
2023-10-08 18:17:14 -0600

    Fix: Removed unnecessary comments

--- client/src/scenes/login/index.jsx
+++ client/src/scenes/login/index.jsx
@@ -72,16 +72,12 @@ function LoginPage() {
             </Button>
           </form>
         </Paper>
-        
-        {/* Contenedor de iconos centrados horizontalmente */}
         <Grid container justifyContent="center" spacing={1}>
-          {/* Primer icono */}
           <Grid item>
             <IconButton onClick={() => dispatch(setMode("dark"))}>
                 <DarkModeOutlined sx={{fontSize: "25px", color: theme.palette.mode == "dark" ? "white" : "gray"}}/>
             </IconButton>
           </Grid>
-          {/* Segundo icono */}
           <Grid item>
             <IconButton onClick={() => dispatch(setMode())}>
                 <LightModeOutlined sx={{fontSize: "25px", color: theme.palette.mode == "dark" ? "gray" : "black"}}/>
@@ -91,61 +87,6 @@ function LoginPage() {
       </Grid>
     </Grid>
   );
-
-
-  return (
-    <Grid container justifyContent="center" alignItems="center" style={{ minHeight: '100vh' }}>
-      <Grid item xs={12} sm={8} md={6} lg={4} sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        <Paper elevation={3} sx={{ padding: 3, width: '100%' }}>
-          <Typography variant="h5">Iniciar Sesión</Typography>
-          <form onSubmit={handleSubmit} style={{ width: '100%', marginTop: 1 }}>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Correo Electrónico"
-              name="email"
-              autoComplete="email"
-              autoFocus
-              value={email}
-              onChange={handleEmailChange}
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Contraseña"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              value={password}
-              onChange={handlePasswordChange}
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ marginTop: 2 }}
-            >
-              Iniciar Sesión
-            </Button>
-          </form>
-        </Paper>
-        {/* Icono SVG justo debajo del bloque de inicio de sesión */}
-        <IconButton onClick={() => dispatch(setMode())}>
-                {theme.palette.mode === "dark" ? (
-                    <DarkModeOutlined sx={{fontSize: "25px"}}/>
-                ) : (
-                    <LightModeOutlined sx={{fontSize: "25px"}}/>
-                )}
-            </IconButton>
-      </Grid>
-    </Grid>
-  )
 }
 
 export default LoginPage;
