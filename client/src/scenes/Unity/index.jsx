874e3468bf79ec3b8365e1ceaa54d297af6be085
Fernando Tenchipe Morales <a01571277@tec.mx>
2024-05-12 22:26:02 -0600

    cambios al index

--- client/src/scenes/Unity/index.jsx
+++ client/src/scenes/Unity/index.jsx
@@ -1,12 +1,9 @@
-import React, { useRef, useEffect } from 'react';
+import styled from "styled-components";
 import { Unity, useUnityContext } from "react-unity-webgl";
+import React, { Fragment } from "react";
 import "./TemplateData/style.css";
 
-function Juego() {
-  const canvasRef = useRef(null);
-  const loadingBarRef = useRef(null);
-  const progressBarFullRef = useRef(null);
-  const fullscreenButtonRef = useRef(null);
+export function Juego() {
 
   const {unityProvider} = useUnityContext({
     loaderUrl: "Build/public.loader.js",
@@ -15,34 +12,13 @@ function Juego() {
     codeUrl: "Build/public.wasm",
   });
 
-  useEffect(() => {
-    var canvas = canvasRef.current;
-    var loadingBar = loadingBarRef.current;
-    var progressBarFull = progressBarFullRef.current;
-    var fullscreenButton = fullscreenButtonRef.current;
+  return (<Container>
+    <Fragment>
+      <Unity unityProvider={unityProvider} style={{ height: 560, width: 1000 }} />
 
-    // You can now use these variables in your code
-    // ...
-  }, []);
-
-  return (
-    <div id="unity-container" className="unity-desktop">
-      <canvas id="unity-canvas" width="920" height="400" tabIndex="-1" ref={canvasRef}></canvas>
-      <div id="unity-loading-bar" ref={loadingBarRef}>
-        <div id="unity-logo"></div>
-        <div id="unity-progress-bar-empty">
-          <div id="unity-progress-bar-full" ref={progressBarFullRef}></div>
-        </div>
-      </div>
-      <div id="unity-warning"></div>
-      <div id="unity-footer">
-        <div id="unity-webgl-logo"></div>
-        <div id="unity-fullscreen-button" ref={fullscreenButtonRef}></div>
-        <div id="unity-build-title">Adivina Quien Unity</div>
-      </div>
-      <Unity unityProvider={unityProvider} />
-    </div>
-  );
+    </Fragment>
+  </Container>);
 }
-
-export default Juego;
+const Container =styled.div`
+  height:100vh;
+`
