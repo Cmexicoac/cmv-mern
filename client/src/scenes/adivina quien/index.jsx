cd319fae9ad0539555cffde16136247b5d2b7229
Fernando Tenchipe Morales <a01571277@tec.mx>
2024-05-15 13:21:41 -0600

    boton de fullscreen

--- client/src/scenes/adivina quien/index.jsx
+++ client/src/scenes/adivina quien/index.jsx
@@ -1,12 +1,18 @@
-import styled from "styled-components";
 import { Unity, useUnityContext } from "react-unity-webgl";
-import React from "react";
+import React, { useRef } from "react";
+import styled from 'styled-components';
 
 const CenteredContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   height: 100vh; // This will make the container take up the full viewport height
+`;
+
+const FullScreenButton = styled.button`
+  position: absolute;
+  bottom: 60px;
+  right: 170px;
 `;
 
 export function Juego() {
@@ -17,9 +23,24 @@ export function Juego() {
     codeUrl: "/juegos/Build/juegos.wasm",
   });
 
+  const unityRef = useRef(null);
+
+  const handleFullScreen = () => {
+    if (unityRef.current.requestFullscreen) {
+      unityRef.current.requestFullscreen();
+    } else if (unityRef.current.mozRequestFullScreen) { /* Firefox */
+      unityRef.current.mozRequestFullScreen();
+    } else if (unityRef.current.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+      unityRef.current.webkitRequestFullscreen();
+    } else if (unityRef.current.msRequestFullscreen) { /* IE/Edge */
+      unityRef.current.msRequestFullscreen();
+    }
+  };
+
   return (
     <CenteredContainer>
-      <Unity unityProvider={unityProvider} style={{ height: 400, width: 920 }} />
+      <Unity unityProvider={unityProvider} style={{ height: 400, width: 920 }} ref={unityRef} />
+      <FullScreenButton onClick={handleFullScreen}>Pantalla completa</FullScreenButton>
     </CenteredContainer>
   );
-} 
+}
